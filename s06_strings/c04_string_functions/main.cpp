30b00e6c601bf4078608a3169063fd01dfb6c3a4
Bryan-Suelo <bryanmker.h@gmail.com>
2023-02-02 00:46:04 -0700

    String comparison

---
+++ s06_strings/c04_string_functions/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    /* String Comparison */
+    char s1[20] = "hello";
+    char s2[10] = "Hello";
+
+    cout << strcmp(s1,s2) << endl;
+    
+    return 0;
+}
