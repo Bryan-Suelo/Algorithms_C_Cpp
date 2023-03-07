b9c00af3ac3c3c910ab14788ab6f88587a2211ec
Bryan-Suelo <bryanmker.h@gmail.com>
2023-03-07 00:02:23 -0700

    Exceptions explained

---
+++ s12_exception_handling/c02_throw_catch_functions/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int division(int a, int b)
+{
+    if(b==0){
+        throw 1;
+    }
+    return a/b;
+}
+
+int main()
+{
+    int x=10;
+    int y=1;
+    int z;
+
+    try{
+        z = division(x,y);
+        cout << z << endl;
+    }
+    catch(int e){
+        cout << "Division by zero " << e << endl;
+    }
+}
